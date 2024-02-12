1b46edbca6bb4b1e4a5461c4b18197c422f1bd46
Eric Ethevenaux <eric.ethevenaux@tacton.com>
2024-02-12 17:09:03 +0100

    wip shaders

--- src/webgl-demo.js
+++ src/webgl-demo.js
@@ -11,4 +11,64 @@ function main() {
 
     gl.clearColor(0.0,0.0,0.0,1.0); // Set clear color to black, fully opaque
     gl.clear(gl.COLOR_BUFFER_BIT); // Clear the color buffer with specified clear color
+
+    // Vertex shader program
+    const vsSource = `
+    attribute vec4 aVertexPosition;
+    uniform mat4 uModelViewMatrix;
+    uniform mat4 uProjectionMatrix;
+    void main() {
+    gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+    }
+    `;
+
+    // Fragment shader program
+    const fsSource = `
+    void main() {
+      gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    }
+    `;
+
+    const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
+    const programInfo = {
+        program: shaderProgram,
+        attribLocations: {
+            vertexPosition: gl.getAttribLocation(shaderProgram, "aVertexPosition"),
+        },
+        uniformLocations: {
+            projectionMatrix: gl.getUniformLocation(shaderProgram, "uProjectionMatrix"),
+            modelViewMatrix: gl.getUniformLocation(shaderProgram, "uModelViewMatrix"),
+        },
+    };
+}
+
+function initShaderProgram(gl, vsSource, fsSource) {
+    const vs = loadShader(gl, gl.VERTEX_SHADER, vsSource);
+    const fs = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
+
+    if (!vs || !fs) {
+        return;
+    }
+
+    const shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+        alert(`Error initializing shader program: ${gl.getProgramInfoLog(shaderProgram)}`);
+        return;
+    }
+}
+
+function loadShader(gl, type, source) {
+    const shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        alert(`Error compiling shaders: ${gl.getShaderInfoLog(shader)}`);
+        gl.deleteShader(shader);
+        return;
+    }
+    return shader;
 }
