5d17c250cbc41190932e8a4f1d3ae7daa0a163af
Moreliaa <eth_er@gmx.de>
2024-02-16 17:17:59 +0100

    ref

--- src/rotating-square/rs_demo.js
+++ src/rotating-square/rs_demo.js
@@ -1,5 +1,6 @@
 import { initBuffers } from "./rs_init-buffers.js";
 import { drawScene } from "./rs_draw-scene.js";
+import { initShaderProgram } from "../init-shader-program.js";
 
 export function runRotatingSquareDemo() {
     let squareRotation = 0.0; // rotation in rad
@@ -77,37 +78,4 @@ export function runRotatingSquareDemo() {
 
     requestAnimationFrame(render);
 
-}
-
-function initShaderProgram(gl, vsSource, fsSource) {
-    const vs = loadShader(gl, gl.VERTEX_SHADER, vsSource);
-    const fs = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
-
-    if (!vs || !fs) {
-        return;
-    }
-
-    const shaderProgram = gl.createProgram();
-    gl.attachShader(shaderProgram, vs);
-    gl.attachShader(shaderProgram, fs);
-    gl.linkProgram(shaderProgram);
-
-    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-        alert(`Error initializing shader program: ${gl.getProgramInfoLog(shaderProgram)}`);
-        return;
-    }
-
-    return shaderProgram;
-}
-
-function loadShader(gl, type, source) {
-    const shader = gl.createShader(type);
-    gl.shaderSource(shader, source);
-    gl.compileShader(shader);
-    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-        alert(`Error compiling shaders: ${gl.getShaderInfoLog(shader)}`);
-        gl.deleteShader(shader);
-        return;
-    }
-    return shader;
 }
