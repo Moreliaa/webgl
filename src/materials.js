cb372003099174dc6612cde79ec08c3ef1dd0af5
Moreliaa <eth_er@gmx.de>
2024-04-17 18:37:56 +0200

    add materials

---
+++ src/materials.js
@@ -0,0 +1,21 @@
+export default function getDefaultMaterials() {
+    // http://devernay.free.fr/cours/opengl/materials.html
+    const shininessFactor =  128;
+    let mats = [
+        {
+            name: "Emerald",
+            ambient: vec3.fromValues(0.0215,0.1745,0.0215),
+            diffuse: vec3.fromValues(0.075680,0.61424,0.07568),
+            specular: vec3.fromValues(0.633,0.727811,0.633),
+            shininess: 0.6 * shininessFactor,
+        },
+        {
+            name: "Gold",
+            ambient: vec3.fromValues(0.24725,0.1995,0.0745),
+            diffuse: vec3.fromValues(0.75164,0.60648,0.22648),
+            specular: vec3.fromValues(0.628281,0.555802,0.366065),
+            shininess: 0.4 * shininessFactor,
+        },
+    ];
+    return mats;
+}
--- src/webgl_demo/settings.js
+++ src/webgl_demo/settings.js
@@ -1,3 +1,5 @@
+import getDefaultMaterials from "../materials.js";
+
 export default class Settings {
     constructor () {
         this.ambientColor = vec3.fromValues(0.3, 0.8, 0.8);
@@ -49,10 +51,7 @@ export default class Settings {
         for (let v in controls_colors) {
             let ctrl = document.getElementById(v);
             let prop = this[controls_colors[v]];
-            let hex_r = (256 * prop[0] - 1).toString(16);
-            let hex_g = (256 * prop[1] - 1).toString(16);
-            let hex_b = (256 * prop[2] - 1).toString(16);
-            ctrl.value = `#${hex_r[0]}${hex_r[1]}${hex_g[0]}${hex_g[1]}${hex_b[0]}${hex_b[1]}`
+            ctrl.value = colorVec3ToHexString(prop);
             ctrl.onchange = (event) => {
                 let val = event.target.value;
                 let hex_r = (parseInt(`${val[1]}${val[2]}`, 16) + 1) / 256;
@@ -96,6 +95,29 @@ export default class Settings {
         document.getElementById("ctrl_texture").onchange = (event) => {
             this.isTextured = event.target.checked;
         };
+
+        let defaultMaterials = getDefaultMaterials();
+        let materialDivContents = "";
+        for (let m of defaultMaterials) {
+            let radio = `<input id="ctrl_mat_${m.name}" type="radio" name="material">`;
+            let label = `<label for="ctrl_mat_${m.name}">${m.name}</label>`
+            materialDivContents += radio + label;
+        }
+        let materialDiv = document.getElementById("defaultMaterials");
+        materialDiv.innerHTML = materialDivContents;
+        for (let m of defaultMaterials) {
+            let ctrl = document.getElementById(`ctrl_mat_${m.name}`);
+            ctrl.onclick = (event) => {
+                document.getElementById("ctrl_ambientColor").value = colorVec3ToHexString(m.ambient);
+                document.getElementById("ctrl_diffuseColor").value = colorVec3ToHexString(m.diffuse);
+                document.getElementById("ctrl_specularColor").value = colorVec3ToHexString(m.specular);
+                document.getElementById("ctrl_shininess").value = m.shininess;
+                this.ambientColor = m.ambient;
+                this.diffuseColor = m.diffuse;
+                this.specularColor = m.specular;
+                this.shininess = m.shininess;
+            };
+        }
         
     }
 
@@ -107,4 +129,11 @@ export default class Settings {
     isBlinnPhongShading() {
         return this.shadingStyle === this.shadingStyles_enum.blinnphong;
     }
+}
+
+function colorVec3ToHexString(vec) {
+    let hex_r = (256 * vec[0] - 1).toString(16);
+    let hex_g = (256 * vec[1] - 1).toString(16);
+    let hex_b = (256 * vec[2] - 1).toString(16);
+    return `#${hex_r[0]}${hex_r[1]}${hex_g[0]}${hex_g[1]}${hex_b[0]}${hex_b[1]}`;
 }
