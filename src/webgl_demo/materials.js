23c57a1da4fe7a8add6b4072b8963be80ec8402a
Moreliaa <eth_er@gmx.de>
2024-04-18 16:54:10 +0200

    add white material

--- src/webgl_demo/materials.js
+++ src/webgl_demo/materials.js
@@ -3,6 +3,13 @@ export default function getDefaultMaterials() {
     const shininessFactor =  128;
     let mats = [
         {
+            name: "White",
+            ambient: vec3.fromValues(1.0,1.0,1.0),
+            diffuse: vec3.fromValues(1.0,1.0,1.0),
+            specular: vec3.fromValues(1.0,1.0,1.0),
+            shininess: 1 * shininessFactor,
+        },
+        {
             name: "Emerald",
             ambient: vec3.fromValues(0.0215,0.1745,0.0215),
             diffuse: vec3.fromValues(0.075680,0.61424,0.07568),
