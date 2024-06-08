17a82d7fc4a9add379d8e5cfb41b0fc07fc22d02
Eric E <eth_er@gmx.de>
2024-06-08 18:08:08 +0200

    add glmatrix directly to project for convenience

---
+++ src/glm_import.js
@@ -0,0 +1,12 @@
+import * as glm from "./gl-matrix/index.js";
+
+globalThis.glMatrix = glm.glMatrix; 
+globalThis.mat2 = glm.mat2;
+globalThis.mat2d = glm.mat2d;
+globalThis.mat3 = glm.mat3;
+globalThis.mat4 = glm.mat4;
+globalThis.quat = glm.quat;
+globalThis.quat2 = glm.quat2;
+globalThis.vec2 = glm.vec2;
+globalThis.vec3 = glm.vec3;
+globalThis.vec4 = glm.vec4;
